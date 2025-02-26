604cc625574afb647bfb965ccba274f7d4b28df7
xirzo <easttem@gmail.com>
2025-02-26 23:37:35 +0300

    refactor: math extracted

--- lib/visual/src/visual.cpp
+++ lib/visual/src/visual.cpp
@@ -9,34 +9,9 @@
 #include <set>
 #include <string>
 
+#include "distance_math.cpp"
+
 using namespace ogdf;
-
-long double rad(const long double& degree) {
-    long double one_deg = (M_PI) / 180;
-    return (one_deg * degree);
-}
-
-long double distance(long double lat1, long double long1, long double lat2,
-                     long double long2) {
-    lat1 = rad(lat1);
-    long1 = rad(long1);
-    lat2 = rad(lat2);
-    long2 = rad(long2);
-
-    long double dlong = long2 - long1;
-    long double dlat = lat2 - lat1;
-
-    long double ans =
-        pow(sin(dlat / 2), 2) + cos(lat1) * cos(lat2) * pow(sin(dlong / 2), 2);
-
-    ans = 2 * asin(sqrt(ans));
-
-    constexpr long double R = 6371;
-
-    ans = ans * R;
-
-    return ans;
-}
 
 edge_pair::edge_pair(const std::string& c1, const std::string& c2) {
     if (c1 < c2) {
