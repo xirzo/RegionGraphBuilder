abec3568369bc74109cca866e8fa9b46911513a7
xirzo <easttem@gmail.com>
2025-02-26 12:20:14 +0300

    refactor: graph_builder caching

--- lib/graph_builder/include/graph_builder.h
+++ lib/graph_builder/include/graph_builder.h
@@ -3,23 +3,22 @@
 
 #include <expected>
 #include <string>
-#include <unordered_map>
-
-#include "country.h"
 
 class graph_builder {
 public:
-    enum class error { region_codes_failed, countries_failed };
+    enum class error {
+        region_codes_failed,
+        countries_failed,
+        countries_write_failed,
+        read_region_file_error
+    };
 
     graph_builder(const std::string geo_data_api_key)
         : geo_data_api_key_(std::move(geo_data_api_key)) {}
 
-    error build(std::string_view region, std::string& error_details);
+    error build(const std::string& region, std::string& error_details);
 
 private:
     const std::string geo_data_api_key_;
-
-    std::expected<std::unordered_map<std::string, country>, graph_builder::error>
-    fetch_countries(std::string_view region, std::string& error_details);
 };
 #endif  // !GRAPH_BUILDER_H
--- lib/graph_builder/src/graph_builder.cpp
+++ lib/graph_builder/src/graph_builder.cpp
@@ -1,46 +1,81 @@
 #include "graph_builder.h"
 
+#include <filesystem>
 #include <format>
+#include <iostream>
+#include <nlohmann/json.hpp>
 #include <unordered_map>
 
+#include "country.h"
 #include "fetch.h"
-
-graph_builder::error graph_builder::build(std::string_view region,
-                                          std::string& error_details) {
-    auto countries_result = fetch_countries(region, error_details);
-
-    if (!countries_result) {
-        return countries_result.error();
-    }
-
-    std::unordered_map<std::string, std::string> name_to_iso_code;
-
-    for (const auto& [_, country] : countries_result.value()) {
-        name_to_iso_code[country.name] = country.iso_code;
-    }
-
-    return {};
-}
+#include "json_file.h"
+#include "nlohmann/json_fwd.hpp"
 
 std::expected<std::unordered_map<std::string, country>, graph_builder::error>
-graph_builder::fetch_countries(std::string_view region, std::string& error_details) {
+fetch_countries(std::string_view region, std::string& error_details,
+                const std::string& geo_data_api_key) {
     auto region_codes_result = fetch::fetch_region_codes(std::string(region));
 
     if (!region_codes_result) {
         error_details =
             std::format("region codes fetch error: {} (region: {})",
                         static_cast<int>(region_codes_result.error()), region);
-        return std::unexpected(error::region_codes_failed);
+        return std::unexpected(graph_builder::error::region_codes_failed);
     }
 
     auto countries_result =
-        fetch::fetch_countries(geo_data_api_key_, region_codes_result.value());
+        fetch::fetch_countries(geo_data_api_key, region_codes_result.value());
 
     if (!countries_result) {
         error_details = std::format("countries fetch error: {} (region: {})",
                                     static_cast<int>(countries_result.error()), region);
-        return std::unexpected(error::countries_failed);
+        return std::unexpected(graph_builder::error::countries_failed);
     }
 
     return countries_result.value();
+}
+
+graph_builder::error graph_builder::build(const std::string& region,
+                                          std::string& error_details) {
+    std::unordered_map<std::string, country> countries;
+
+    if (!std::filesystem::exists(region)) {
+        auto countries_result = fetch_countries(region, error_details, geo_data_api_key_);
+
+        if (!countries_result) {
+            return countries_result.error();
+        }
+
+        countries = countries_result.value();
+
+        nlohmann::json j_umap(countries);
+
+        auto write_result = json_file::write(region, j_umap);
+
+        if (!write_result) {
+            error_details = std::format("countries write to file error: {}",
+                                        static_cast<int>(write_result.error()));
+            return error::countries_write_failed;
+        }
+    } else {
+        auto region_result = json_file::read(region);
+
+        if (!region_result) {
+            return error::read_region_file_error;
+        }
+
+        countries = region_result.value();
+    }
+
+    std::unordered_map<std::string, std::string> name_to_iso_code;
+
+    for (const auto& [_, country] : countries) {
+        name_to_iso_code[country.name] = country.iso_code;
+    }
+
+    for (const auto& [_, country] : countries) {
+        std::cout << country.name << std::endl;
+    }
+
+    return {};
 }
